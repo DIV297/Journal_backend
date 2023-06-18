4d6a24b32ddec9d026761a3b0b0e8c0361dfb024
DIV297 <91648671+DIV297@users.noreply.github.com>
2023-06-18 10:56:09 +0530

    dsfsd

--- db.js
+++ db.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose')
 const connectToMongo=()=>{
-mongoose.connect("mongodb+srv://divbajaj297:Divanshbajaj297$@cluster0.lwlgrra.mongodb.net/?retryWrites=true&w=majority/journal");
+mongoose.connect("mongodb+srv://divbajaj297:Divanshbajaj297$@cluster0.lwlgrra.mongodb.net/?retryWrites=true/journal");
 console.log("connected to db");
 }
 connectToMongo()
